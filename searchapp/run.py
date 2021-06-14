096348691a1157e3d0009a59bd531ea1c352b16d
s_zolotorevich <s_zolotorevich@wargaming.net>
2021-06-14 12:53:08 +0300

    Lesson 2: Searching

--- searchapp/run.py
+++ searchapp/run.py
@@ -1,8 +1,15 @@
+import logging
+
 from searchapp.app.app import app
 
 
 def main():
+    es_logger = logging.getLogger('elasticsearch')
+    es_logger.addHandler(logging.StreamHandler())
+    es_logger.setLevel(logging.DEBUG)
+
     app.run(debug=True)
+
 
 if __name__ == '__main__':
     main()
