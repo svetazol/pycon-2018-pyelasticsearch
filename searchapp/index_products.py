cf0aab13e3b3f4e673a8362ef395c02ebe8e6a24
s_zolotorevich <s_zolotorevich@wargaming.net>
2021-06-14 15:25:52 +0300

    Lesson 3: Analysis

--- searchapp/index_products.py
+++ searchapp/index_products.py
@@ -13,8 +13,35 @@ def main():
     es.indices.create(
         index=INDEX_NAME,
         body={
-            'mappings': {},
-            'settings': {},
+            'mappings': {
+                DOC_TYPE: {  # This mapping applies to products.
+                    'properties': {  # Just a magic word.
+                        'name': {  # The field we want to configure.
+                            'type': 'text',  # The kind of data we’re working with.
+                            'fields': {  # create an analyzed field.
+                                'english_analyzed': {
+                                    # Name that field `name.english_analyzed`.
+                                    'type': 'text',  # It’s also text.
+                                    'analyzer': 'custom_english_analyzer',
+                                    # And here’s the analyzer we want to use.
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            'settings': {
+                'analysis': {  # magic word.
+                    'analyzer': {  # yet another magic word.
+                        'custom_english_analyzer': {  # The name of our analyzer.
+                            'type': 'english',
+                            # The built in analyzer we’re building on.
+                            'stopwords': ['made', '_english_'],
+                            # Our custom stop words, plus the defaults.
+                        },
+                    },
+                },
+            },
         },
     )
     products_to_index(es, all_products())
